e7f99606a7c8fe3f7c84c673d8d6b727c307d615
Carlos Ramirez <carlos.ramirez16031@gmail.com>
2023-11-22 20:32:55 -0500

    Login, Logout, Registro Usuarios

--- website/views.py
+++ website/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
+def inicio(request):
+    # Comprueba si estás iniciando sesión
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # Autenticar
+        user = authenticate(request, username=username, password=password)
+        if user is not None: 
+            login(request, user)
+            messages.success(request, "¡Has iniciado sesión!")
+            return redirect('inicio')
+        else:
+            messages.success(request, "Se ha producido un error al iniciar sesión, inténtalo de nuevo...")
+            return redirect('inicio')
+    else:
+        return render(request, 'inicio.html', {})
+
+#def login_user(request):
+    #pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"¡Has sido desconectado!..." )
+    return redirect('inicio')
+
+
+def register_user(request):
+    return render (request, 'register.html', {})
 # Create your views here.
